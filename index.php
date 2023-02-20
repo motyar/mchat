3f31cbe8d836cb88e62d55e9e0b78bbc352ea358
Motyar <dharmmotyar@gmail.com>
2023-02-21 00:57:20 +0530

    tailwind

--- index.php
+++ index.php
@@ -1,60 +1,45 @@
 <!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<style>
-body
-{
-	width: 80%;
-	margin: 0 auto;
-}
-#chats
-{
-	height: 500px;
-	overflow-y: scroll;
-}
-#msg
-{
-	font-size: 25px;
-	width: 99%;
-	border: 1px solid #DDD;
-	border-radius: 5px;
-	-moz-border-radius: 5px;
-	font-family: Arial, Helvetica, sans-serif;
-	margin: 5px auto 20px;
-	padding: 5px;
-	border-image: initial;
-}
-.msgln
-{
-	padding: 3px;
-}
-.msgln:nth-child(odd)
-{
-	background-color: #EAEAEA;
-}
-footer
-{
-	position: fixed;
-	bottom: 10px;
-}
-</style>
-</head>
-<body>
-<?php
-	$room = 'roomname';
-?>
-<div id="chats"></div>
-<form onsubmit="javascript:sendMsg();return false;">
-	<input type="text" name="text" id="msg" autocomplete="off" />
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Chat Interface</title>
+    <!-- Include Tailwind CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.7/tailwind.min.css"
+      
+    />
+  </head>
+  <body>
+    <div class="h-screen flex flex-col">
+      <div class="flex-1 bg-gray-100 p-6 flex flex-col justify-end">
+        <div class="flex flex-col gap-2" id="chats">
+          
+          <!-- Add more messages here as needed -->
+        </div>
+      </div>
+	<form onsubmit="sendMsg();return false;">
+      <div class="bg-white p-4 flex">
+        <input
+          type="text"
+	   id="msg"
+          placeholder="Type your message here..."
+          class="flex-1 border rounded-lg py-2 px-4 mr-4"
+        />
+        <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-4">
+          Send
+        </button>
+      </div>
 </form>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    </div>
+  </body>
 <script>
 var username = '';
 
 if(!username)
 {
-	var username = prompt("Hey there, good looking stranger!  What's your name?", "");
+	username = prompt("Hey there, good looking stranger!  What's your name?", "");
 }
 
 function sendMsg(){
@@ -73,35 +58,33 @@ function sendMsg(){
 		return;
 	}
 	
-	document.getElementById("chats").innerHTML+=strip('<div class="msgln"><b>'+username+'</b>: '+msg+'<br/></div>');
-	$("#chats").animate({ scrollTop: 2000 }, 'normal');
 
-	$.get('/server.php?msg='+msg+'&user='+username+'&room=<?php echo $room; ?>', function(data)
-	{
-		document.getElementById("msg").value = '';
-	});
+document.querySelector("#msg").value='';
+	fetch('?msg='+username+ " : " +msg);
 }
 
-var old = '';
-var source = new EventSource('/server.php?room=<?php echo $room; ?>');
+var source = new EventSource('?server=yes');
 
+var old = '';
 source.onmessage = function(e)
 {
 	if(old!=e.data){
-		document.getElementById("chats").innerHTML='<span>'+e.data+'</span>';
-		old = e.data;
+
+
+		const [un, text] = e.data.split(' : ');
+
+		const mtd  = `<span class="font-bold">${un}</span> - ${text}`;
+
+	if(e.data.includes(username+" :")){
+		document.getElementById("chats").innerHTML+='<div class="bg-gray-300 text-gray-700 rounded-lg p-2 max-w-max self-end">'+mtd+'</div>';
+	}else{
+
+		document.getElementById("chats").innerHTML+='<div class="bg-blue-500 text-white rounded-lg p-2 max-w-max">'+mtd+'</div>';
+	}
+	window.scrollTo(0, document.body.scrollHeight); 
+	old = e.data;
 	}
 };
 
-function strip(html)
-{
-	var tmp = document.createElement("DIV");
-	tmp.innerHTML = html;
-	return tmp.textContent||tmp.innerText;
-}
 </script>
-<footer>
-Created by <a href="http://twitter.com/motyar">@motyar</a> | <a href="http://motyar.blogspot.com/">blog</a> | <a href="http://news.ycombinator.com/item?id=3509146">discuss</a>
-</footer>
-</body>
 </html>
